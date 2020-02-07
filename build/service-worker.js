328084e2bfa2f30a17b7a783d69dc406282ab1c9
Ahmed Elbanna <ahmedelpna@gmail.com>
2020-02-07 10:42:36 +0200

    new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18973c23bc0b4a9bd4c024ee6d18ab18.js"
+  "/precache-manifest.8de37763a00e9e11ecb00f38c4d1616d.js"
 );
 
 self.addEventListener('message', (event) => {
