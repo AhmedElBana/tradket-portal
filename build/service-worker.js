a4f06705030c2b87b67b4d14014120d584d6f40d
ahmedelbanna <ahmedelbanna@weaccept.co>
2021-11-04 04:25:55 +0200

    new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21b16656b0552a1fe40ba83d25fa2559.js"
+  "/precache-manifest.627e0e4546068bafa2ae4987ba2a81fe.js"
 );
 
 self.addEventListener('message', (event) => {
