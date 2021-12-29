56827509a719068746eb20000f96a328bfcc2666
ahmedelbanna <ahmedelbanna@weaccept.co>
2021-12-29 17:16:33 +0200

    complate new order with scan

--- src/views/Pages/OrderNew/OrderNew.js
+++ src/views/Pages/OrderNew/OrderNew.js
@@ -10,6 +10,7 @@ import VInput from 'react-validation/build/input';
 import VSelect from 'react-validation/build/select';
 import VButton from 'react-validation/build/button';
 
+import successImg from "./../../../assets/img/success.png"
 import './OrderNew.scss'
 import { dependencies } from '../../../tools/Dependencies';
 
@@ -50,16 +51,33 @@ const phoneNumber = (value) => {
   }
 }
 const OrderNew = () => {
+    const [status, setStatus] = useStateWithCallbackLazy("");
+    const [error, setError] = useStateWithCallbackLazy("");
     const [waiting, setWaiting] = useStateWithCallbackLazy(false);
     const [products, setProducts] = useStateWithCallbackLazy({});
     const [customer, setCustomer] = useStateWithCallbackLazy({"name": "", "phone": ""});
     const [method, setMethod] = useStateWithCallbackLazy("cash");
-
+    const [payed, setPayed] = useStateWithCallbackLazy(0);
+    const [payedChange, setpayedChange] = useStateWithCallbackLazy(false);
+    const persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
+    arabicNumbers  = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
+    fixNumbers = function (str)
+    {
+      if(typeof str === 'string')
+      {
+        for(var i=0; i<10; i++)
+        {
+          str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+        }
+      }
+      return str;
+    };
     useScanDetection({
         onComplete: (code) => {onCompleteScan(code)},
         minLength: 6 // EAN13
     });
     const onCompleteScan = (code) => {
+        code = fixNumbers(code);
         let new_products = {...products}
         if(new_products[code]){
             new_products[code]["quantity"] += 1;
@@ -103,7 +121,7 @@ const OrderNew = () => {
     const handlePriceChange = (e, code) => {
         if(products[code]){
             let new_products = {...products}
-            new_products[code]["data"]["price"] = e.target.value;
+            new_products[code]["data"]["price"] = fixNumbers(e.target.value);
             setProducts({...new_products})
         }
     }
@@ -116,14 +134,23 @@ const OrderNew = () => {
     }
     const handleCustomerChange = (e, field) => {
         let new_customer = {...customer}
-        new_customer[field] = e.target.value;
+        new_customer[field] = fixNumbers(e.target.value);
         setCustomer({...new_customer})
     }
     const handleMethodChange = (e) => {
         setMethod(e.target.value)
     }
+    const handlePayedChange = (e) => {
+        let payedValue = fixNumbers(e.target.value);
+        if(payedChange){
+            setPayed(payedValue)
+        }else{
+            setpayedChange(true, ()=>{
+                setPayed(payedValue)
+            })
+        }
+    }
     const loadProduct = (product_id) => {
-        console.log(products)
         //request data
         let config = {
             headers: {
@@ -139,7 +166,6 @@ const OrderNew = () => {
             config,
             (resp) => {
                 let new_products = {...products};
-                console.log(new_products)
                 if(resp.data && resp.data.data && resp.data.data.result && resp.data.data.result[0]){
                     new_products[product_id]= {
                         "quantity": 1,
@@ -178,7 +204,6 @@ const OrderNew = () => {
     const handleNewOrderSubmit = (e) => {
         e.preventDefault();
         setWaiting(true, () => {
-            console.log(products)
             let products_arr = [];
             let new_products_arr = [];
             Object.keys(products).map((product_id) => {
@@ -200,85 +225,79 @@ const OrderNew = () => {
             let orderObj = {
                 "customerName": customer.name,
                 "customerPhone": customer.phone,
-                "products": products_arr,
-                "new_products": new_products_arr,
-                "payed": 0,
+                "payed": payed,
                 "method": method,
-                "promo": false,
+                "promo": "false",
                 "branch_id": dependencies.getUserData().branches[0]
             };
-            console.log(orderObj)
+            if(products_arr.length > 0){
+                orderObj["products"] = products_arr;
+            }
+            if(new_products_arr.length > 0){
+                orderObj["new_products"] = new_products_arr;
+            }
             let orderData = JSON.stringify(orderObj);
-            // let config = {
-            //     headers: {
-            //     //"Cache-Control": "no-cache",
-            //     "Content-Type": "application/json",
-            //     "x-auth": auth.getMerchantToken()
-            //     }
-            // }
-            // httpClient.post(
-            //     "/api/order/create",
-            //     config,
-            //     orderData,
-            //     (resp) => {
-                    
-            //     },
-            //     (error) => {
-                    
-            //     }
-            // )
+            let config = {
+                headers: {
+                //"Cache-Control": "no-cache",
+                "Content-Type": "application/json",
+                "x-auth": auth.getMerchantToken()
+                }
+            }
+            httpClient.post(
+                "/api/order/create",
+                config,
+                orderData,
+                (resp) => {
+                    setStatus("success");
+                    setWaiting(false);
+                    setTimeout(() => { window.location.reload() }, 2000)
+                },
+                (error) => {
+                    let errorMessage = "";
+                    if(error.response){
+                        if(error.response.status === 401){
+                            //this.setState({logout: true});
+                            errorMessage = "قم بتسجيل الخروج و اعادة الدخول مرة اخري.";
+                        }else if(error.response.status === 400){
+                            errorMessage = error.response.data.message;
+                        }else{
+                            errorMessage = "حدث خطا ما ، يرجي المحاوله في وقت لاحق.";
+                        }
+                    }else{
+                        errorMessage = "حدث خطا ما ، يرجي المحاوله في وقت لاحق.";
+                    }
+                    setStatus("error");
+                    setError(errorMessage);
+                    setWaiting(false);
+                }
+            )
         })
     }
+    let order_total = 0;
+    Object.keys(products).map((product_id) => {
+        order_total += Number(products[product_id].data.price) * Number(products[product_id].quantity)
+    })
+    if(order_total && order_total != payed && !payedChange){setPayed(order_total)}
     return (
-        <div>
+        <div autoFocus={true}>
             <p className="new_order_h">طلب جديد</p>
             {waiting ? 
                 <div className="new_order_wating_div">
                     <Waiting height="100px" />
                 </div>
-            :
+            :status == "success" ? 
+                <div className="new_order_succes_div">
+                    <img src={successImg} alt="succes"/>
+                    <p>تم إنشاء الطلب بنجاح</p>
+                </div>
+            :status == "error" ? 
+            <div className="new_order_succes_div">
+                <Alert color="danger">{error}</Alert>
+            </div>
+        :
                 <VForm onSubmit={handleNewOrderSubmit}>
                     <div className="products_full_div">
-                        <div className="new_order_customer">
-                            <Row>
-                                <Col sm="4" className="inputeDiv">
-                                    <div className="tradketInputGroup full_width">
-                                        <VInput type="text" className="tradket_b_i "
-                                            autoComplete="off"
-                                            name="customer_name"
-                                            value={customer.name}
-                                            onChange={(e) => handleCustomerChange(e, "name")}
-                                            validations={[required]}
-                                            placeholder="اسم العميل"
-                                        />
-                                    </div>
-                                </Col>
-                                <Col sm="4" className="inputeDiv">
-                                    <div className="tradketInputGroup full_width">
-                                        <VInput type="text" className="tradket_b_i "
-                                            autoComplete="off"
-                                            name="customer_phone"
-                                            value={customer.phone}
-                                            onChange={(e) => handleCustomerChange(e, "phone")}
-                                            validations={[required, phoneNumber]}
-                                            placeholder="رقم الهاتف"
-                                        />
-                                    </div>
-                                </Col>
-                                <Col sm="4" className="inputeDiv">
-                                    <div className="tradketInputGroup full_width">
-                                        <VSelect type="select" name="type" className="tradket_b_s"
-                                            value={method}
-                                            onChange={(e) => handleMethodChange(e)}
-                                            validations={[required]} 
-                                        >
-                                            <option value="cash">كاش</option>
-                                            <option value="card">بطاقة بنكية</option>
-                                        </VSelect>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </div>
                         {Object.keys(products).map((product_id) => {
                             return (
                                 <div className="product_div" key={product_id}>
@@ -349,17 +368,73 @@ const OrderNew = () => {
                                 </div>
                             )
                         })}
-                        <div className="new_order_total_div">
-                            <Row>
-                                <Col sm="6">
-                                    <p className="total_p"><span>الاجمالي : </span> 400 </p>
-                                </Col>
-                            </Row>
-                        </div>
+                        {Object.keys(products).length != 0 &&
+                            <div className="new_order_total_div">
+                                <div className="new_order_customer">
+                                    <Row>
+                                        <Col sm="4" className="inputeDiv">
+                                            <div className="tradketInputGroup full_width">
+                                                <VInput type="text" className="tradket_b_i "
+                                                    autoComplete="off"
+                                                    name="customer_name"
+                                                    value={customer.name}
+                                                    onChange={(e) => handleCustomerChange(e, "name")}
+                                                    validations={[required]}
+                                                    placeholder="اسم العميل"
+                                                />
+                                            </div>
+                                        </Col>
+                                        <Col sm="4" className="inputeDiv">
+                                            <div className="tradketInputGroup full_width">
+                                                <VInput type="text" className="tradket_b_i "
+                                                    autoComplete="off"
+                                                    name="customer_phone"
+                                                    value={customer.phone}
+                                                    onChange={(e) => handleCustomerChange(e, "phone")}
+                                                    validations={[required, phoneNumber]}
+                                                    placeholder="رقم الهاتف"
+                                                />
+                                            </div>
+                                        </Col>
+                                        <Col sm="4" className="inputeDiv">
+                                            <div className="tradketInputGroup full_width">
+                                                <VSelect type="select" name="type" className="tradket_b_s"
+                                                    value={method}
+                                                    onChange={(e) => handleMethodChange(e)}
+                                                    validations={[required]} 
+                                                >
+                                                    <option value="cash">كاش</option>
+                                                    <option value="card">بطاقة بنكية</option>
+                                                </VSelect>
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                </div>
+                                <Row>
+                                    <Col sm="4" className="inputeDiv">
+                                        <p className="input_label">المدفوع</p>
+                                        <div className="tradketInputGroup full_width">
+                                            <VInput type="text" className="tradket_b_i "
+                                                autoComplete="off"
+                                                name="payed"
+                                                value={payed}
+                                                onChange={(e) => handlePayedChange(e)}
+                                                validations={[required, NumberV]}
+                                                placeholder="المدفوع"
+                                            />
+                                        </div>
+                                    </Col>
+                                    <Col sm="4">
+                                        <p className="total_p"><span>الاجمالي : </span> {order_total ? order_total : " - "} </p>
+                                    </Col>
+                                </Row>
+                            </div>
+                        }
                     </div>
                     <div className="new_order_footer">
                         {Object.keys(products).length == 0 ?
-                            <button className="btn btn-info" disabled={true}>إكمال الطلب</button>
+                            // <button className="btn btn-info" disabled={true}>إكمال الطلب</button>
+                            null
                         :
                             <VButton className="btn btn-info">إكمال الطلب</VButton>
                         }
